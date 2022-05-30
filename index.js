1c32fb799cff70ed1a7df7e34cdc4001247b9e5e
witerowson <runurowson@gmail.com>
2022-05-30 21:53:06 +0600

    add all order

--- index.js
+++ index.js
@@ -66,11 +66,13 @@ async function run() {
         })
         // to get all order
         app.get('/order', verifyJWT, async (req, res) => {
-            const user = req.body.userEmail;
-            const query = { user: user }
+            const userEmail = req.body.userEmail;
+            const query = { user: userEmail }
             const orders = await orderCollection.find(query).toArray()
             res.send(orders)
         })
+
+
         // to add new order
         app.post('/order', async (req, res) => {
             const order = req.body;
